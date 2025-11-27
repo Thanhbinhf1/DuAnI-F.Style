20d136e3f074de19f2fdd24afbf39c61a3f6ae60
luyen0909 <luyenluongpro0@gmail.com>
2025-11-27 13:02:02 +0700

    up
    up

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -4,8 +4,13 @@ include_once 'Models/Product.php';
 class PageController {
     function home() {
         $productModel = new Product();
-        $dsSanPham = $productModel->getNewProducts(); 
         
+        // Lấy 3 danh sách sản phẩm khác nhau
+        $spMoi = $productModel->getNewProducts();
+        $spHot = $productModel->getHotProducts();
+        $spGiaTot = $productModel->getSaleProducts();
+        
+        // Gửi tất cả sang View
         include_once 'Views/users/page_home.php';
     }
 }
