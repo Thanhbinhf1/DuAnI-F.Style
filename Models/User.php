3bc47d57a2de15c555c9efdfa537b541f69c333c
luyen0909 <luyenluongpro0@gmail.com>
2025-11-27 13:51:10 +0700

    up

--- Models/User.php
+++ Models/User.php
@@ -6,23 +6,22 @@ class User {
         $this->db = new Database();
     }
 
-    // 1. Kiểm tra đăng nhập
-    function checkUser($username, $password) {
-        $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-        return $this->db->queryOne($sql);
+    // 1. Kiểm tra user (Chỉ lấy user ra, việc so sánh mật khẩu để Controller làm)
+    function checkUser($username) {
+        $sql = "SELECT * FROM users WHERE username = ?";
+        return $this->db->queryOne($sql, [$username]);
     }
 
-    // 2. Đăng ký tài khoản mới
+    // 2. Đăng ký (Mật khẩu sẽ được mã hóa bên Controller trước khi gửi vào đây)
     function insertUser($username, $password, $fullname, $email) {
-        $sql = "INSERT INTO users(username, password, fullname, email) 
-                VALUES ('$username', '$password', '$fullname', '$email')";
-        return $this->db->execute($sql);
+        $sql = "INSERT INTO users(username, password, fullname, email) VALUES (?, ?, ?, ?)";
+        return $this->db->execute($sql, [$username, $password, $fullname, $email]);
     }
     
-    // 3. Kiểm tra user đã tồn tại chưa (tránh trùng tên)
+    // 3. Kiểm tra tồn tại
     function checkUserExist($username) {
-        $sql = "SELECT * FROM users WHERE username='$username'";
-        return $this->db->queryOne($sql);
+        $sql = "SELECT * FROM users WHERE username = ?";
+        return $this->db->queryOne($sql, [$username]);
     }
 }
 ?>
