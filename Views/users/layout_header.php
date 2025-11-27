84655d2b9bb8aa1b5b6c0dfcaeeb5071068654ef
Thanhbinhf1 <thanhbinh20082006@gmail.com>
2025-11-27 22:30:25 +0700

    Update layout_header.php

--- Views/users/layout_header.php
+++ Views/users/layout_header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>F.Style Store</title>
-    <link rel="stylesheet" href="./public/Css/style.css">
+    <link rel="stylesheet" href="./public/Css/home.css">
 </head>
 
 <body>
