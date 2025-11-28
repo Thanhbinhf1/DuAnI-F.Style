f4d5154d2e2a6c04bb80fba11dfe430f6a82377d
truongquangquy2512-coder <truongquangquy2512@gmail.com>
2025-11-29 01:36:23 +0700

    jjj

--- Views/users/layout_footer.php
+++
@@ -1,7 +0,0 @@
-</main> <footer> 
-        <div class="footer-content">
-            <p>&copy;  2025 Lương Hữu Luyến - Nguyễn Thanh Bình - Trương Quang Quy - Phạm Xuân Ái</p>
-        </div>
-    </footer>
-</body>
-</html>
