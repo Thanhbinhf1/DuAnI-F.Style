9535bc4974c040ed773d47af9460e370f8a951e3
luyen0909 <luyenluongpro0@gmail.com>
2025-12-01 23:44:20 +0700

    1

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -13,9 +13,9 @@ class PageController {
 
     // Trang chủ
     public function home() {
-        $spMoi    = $this->productModel->getNewProducts();
-        $spHot    = $this->productModel->getHotProducts();
-        $spGiaTot = $this->productModel->getSaleProducts();
+        $spMoi    = $this->productModel->getNewProducts(8);
+        $spHot    = $this->productModel->getHotProducts(8);
+        $spGiaTot = $this->productModel->getSaleProducts(8);
 
         include_once 'Views/users/Page_home.php';
     }
